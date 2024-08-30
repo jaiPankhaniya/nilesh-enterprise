180ae8be7ae96140960759ee188cf346a35023fa
Jay Pankhaniya <39559153+jaiPankhaniya@users.noreply.github.com>
2024-08-30 13:42:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Nilesh Enterprise</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Labs - Design Studio">
+	<meta name="keywords" content="lab, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/logo.png" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<!--Start of Tawk.to Script-->
+	<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(
+			function()
+			{
+				var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+				s1.async=true;
+				s1.src='https://embed.tawk.to/5e98532d35bcbb0c9ab1c035/default';
+				s1.charset='UTF-8';
+				s1.setAttribute('crossorigin','*');
+				s0.parentNode.insertBefore(s1,s0);
+			}
+		)();
+	</script>
+	<!--End of Tawk.to Script-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader">
+			<img src="img/logo.png" alt="">
+			<h2>Loading.....</h2>
+		</div>
+	</div>
+
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="logo">
+			<a href="index.php"><img src="img/logo.png" alt=""></a><!-- Logo -->
+		</div>
+		<!-- Navigation -->
+		<div class="responsive"><i class="fa fa-bars"></i></div>
+		<nav>
+			<ul class="menu-list">
+				<li class="active"><a href="index.php">Home</a></li>
+				 <li><a href="Photos.php">Photos</a>
+				 <li><a href="Videos.php">Videos</a>
+				<li><a href="Products.php">Products</a></li>
+				<li><a href="Services.php">services</a></li>
+				<li><a href="Clients.php">Clients</a></li>
+				<li><a href="T&c.php">T&C </a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+				<li><a href="brochure.pdf">Brochure</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->
+
+
+		<!-- slider -->
+		<div id="hero-slider" class="owl-carousel">
+			<div class="item  hero-item" data-bg="img/01.jpg"></div>
+			<div class="item  hero-item" data-bg="img/02.jpg"></div>
+			<div class="item  hero-item" data-bg="img/03.jpg"></div>
+			<div class="item  hero-item" data-bg="img/04.jpg"></div>
+		</div>
+	</div>
+	<!-- Intro Section -->
+	
+	<!--Page Header-->
+	<div class="page-top-section">
+		<div class="overlay"></div>
+		<div align="center">
+			<div class="page-info">
+				<p><h5>Different type of Blasting Mat Developer</h5></p>
+				<p><h2><b>NileshBhai P. Chandegra</h2></b></p>
+				<p><h5>(Blasting Mat Fabricator)</h5></p>
+			</div>
+		</div>
+	</div>
+
+	
+	<!-- About section -->
+	<div class="about-section">
+		<div class="overlay"></div>
+		<!-- card section -->
+		<div class="card-section">
+			<div class="container">
+				<div class="row">
+					<!-- single card -->
+					<div class="col-md-4 col-sm-6">
+						<div class="lab-card">
+							<h2>Services</h2>
+							<p>We Provides You the Blasting Mats as per your prefered design and requirements.
+							</br></br>Note:- Now My standard Size of Quarter Tyre Mat is (5 Feet x 12 Feet) but as per your requirement we provides preferred size.</p>
+							</br></br><a href="Services.php"><Button><img src="img/read.png"></button></a>
+						</div>
+					</div>
+					<!-- single card -->
+					<div class="col-md-4 col-sm-6">
+						<div class="lab-card">							
+							<h2>Products</h2>
+							<p>We Fabricates the Different Types of  Blasting Mats. 
+								Which Are:
+									</br>(1) Quarter(Woven) tyre Blasting mats.
+									</br>(2) Thread Mat.
+									</br>(3) Sidewall Mat.</br></br>
+							</p></br></br>
+							</a href="Products.php"><button><img src="img/read.png"></button></a>
+						</div>
+					</div>
+					<!-- single card -->
+					<div class="col-md-4 col-sm-12">
+						<div class="lab-card">
+							<h2>Clients</h2>
+							<p>I am official vendor of L & T Ltd and Solar Group Explosives.</p>
+						</br></br></br></br></br></br></br></br>
+						<a href="clients.php"><button><img src="img/read.png"></button></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- card section end-->
+
+
+		<!-- About Business -->
+		<div class="about-contant">
+			<div class="container">
+				<div class="section-title">
+					<p><h2><b>Usage Of Blasting Mat</b></h2></p>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<p>Blasting mats are used when the explosives are detonate at in place like construction sites.The Blasting mat is placed over the blastng to reduce the noise of explosive and reduce the pollution and prevent Rock Flying.The blast mats provided optimal protection and greatly increase the safety of site workers and public. The Blasting mat is Easily hanging up and placed over the blasting are using the Crane.The mayor advantage of blasting mat is is the fact that during explosion the mat cover allows the air pressure and gases through while itself does not move (or hardly) but still containers fly rocks and debris.</p>
+					</div>
+					<div class="col-md-6">
+					<p><img src="img/Mining site.jpg"></p>
+					</div>
+				</div>
+				
+				<!-- popup video -->
+				<div class="intro-video">
+					<p align="center" style="font-size:30px"><b>Play the video to know how blasting mats work.</b></p></br>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<img src="img/Cover.jpg" alt="">
+							<a href="https://www.youtube.com/watch?v=GIyzmlVI4bw" class="video-popup">
+								<i class="fa fa-play"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- About section end -->
+
+	<!-- Services section -->
+	<div class="services-section spad">
+		<div class="container">
+			<div class="section-title dark">
+			</div>
+			
+			<div class="text-center">
+
+			</div>
+		</div>
+	</div>
+	<!-- services section end -->
+
+
+
+	<!-- Promotion section -->
+	<div class="promotion-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<h2>For More Information</h2>
+					<p>Contact Use On the Giver E-mail.</p>
+				</div>
+				<div class="col-md-3">
+					<div class="promo-btn-area">
+						<a href="contact.php" class="site-btn btn-2">Contact Us</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Promotion section end-->
+
+
+	<!-- Contact section -->
+	<div class="contact-section spad fix">
+		<div class="container">
+			<div class="row">
+				<!-- contact info -->
+				<div class="col-md-5 col-md-offset-1 contact-info col-push">
+					<div class="section-title left">
+						<h2>Contact us</h2>
+					</div>
+					<p><h3>Main Office</h3></p>
+					<p class="con-item">Street no-16, <br> New Kumbharwada, <br/> Porbandar, gujarat-360575. </p>
+					<p class="con-item"><b>Phone:</b> +91 9714421999</p>
+					<p class="con-item"><b>Email:</b>   Purshottam.rubber.industry@gmail.com</p>
+				</div>
+				<!-- contact form -->
+				<div class="col-md-6 col-pull">
+					<form class="form-class" action="mailto:purshottam.rubber.industry@gmail.com" id="con_form">
+						<div class="row">
+							<div class="col-sm-6">
+								<input type="text" name="name" placeholder="Your name">
+							</div>
+							<div class="col-sm-6">
+								<input type="text" name="email" placeholder="Your email">
+							</div>
+							<div class="col-sm-12">
+								<input type="text" name="subject" placeholder="Subject">
+								<textarea name="message" placeholder="Message"></textarea>
+								<button class="site-btn">send</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Contact section end-->
+
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<h2>2018 All rights reserved. Designed by <a href="Mailto:Pankhaniyajay@gmail.com" target="_blank">Pankhaniya Jay</a></h2>
+	</footer>
+	<!-- Footer section end -->
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
