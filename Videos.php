00abdd6d1333d53fa374033c7ecd3597de16ef82
Jay Pankhaniya <39559153+jaiPankhaniya@users.noreply.github.com>
2024-08-30 13:42:49 +0530

    added

---
+++ Videos.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Videos | Nilesh-enterprise</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Labs - Design Studio">
+	<meta name="keywords" content="lab, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/logo.png" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,600" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<!--Chat-->
+	<!--Start of Tawk.to Script-->
+	<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(
+			function()
+			{
+				var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+				s1.async=true;
+				s1.src='https://embed.tawk.to/5e98532d35bcbb0c9ab1c035/default';
+				s1.charset='UTF-8';
+				s1.setAttribute('crossorigin','*');
+				s0.parentNode.insertBefore(s1,s0);
+			}
+		)();
+	</script>
+	<!--End of Tawk.to Script-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader">
+			<img src="img/logo.png" alt="">
+			<h2>Loading.....</h2>
+		</div>
+	</div>
+
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="logo">
+			<img src="img/logo.png" alt=""><!-- Logo -->
+		</div>
+		<!-- Navigation -->
+		<div class="responsive"><i class="fa fa-bars"></i></div>
+	<nav>
+			<ul class="menu-list">
+				<li><a href="index.php">Home</a></li>
+				 <li><a href="Photos.php">Photos</a>
+				 <li class="active"><a href="Videos.php">Videos</a>
+				<li><a href="Products.php">Products</a></li>
+				<li><a href="Services.php">services</a></li>
+				<li><a href="Clients.php">Clients</a></li>
+				<li><a href="T&c.php">T&C</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+				<li><a href="brochure.pdf">Brochure</a></li>
+			</ul>	
+		</nav>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Page header -->
+	<div class="page-top-section">
+		<div class="container text-right">
+			<div class="page-info">
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+				<h2><b>Videos<b></h2>
+			</div>
+		</div>
+	</div>
+	<!-- Page header end-->
+
+
+	<!-- page section -->
+	<div class="page-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<!-- Single element -->
+					<div class="element">
+						<p><h3></h3></p>
+						<div class="buttons">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/ezG33vGduio" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 		picture-in-picture" allowfullscreen></iframe>
+							
+							&nbsp;&nbsp;&nbsp;
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/WNAeOy_-8rM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div></br></br>
+						<div class="buttons">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/O7B2iQD8NX4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							
+							&nbsp;&nbsp;&nbsp;
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/8WDqfV4yMX4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div></br></br>
+						<div class="button">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/y3RHOZ9Sjtc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+					
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<h2>2018 All rights reserved. Designed by <a href="Mailto:Pankhaniyajay@gmail.com" target="_blank">Pankhaniya Jay</a></h2>
+	</footer>
+	<!-- Footer section end -->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
